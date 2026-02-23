3cb1e8d9f14af388bbeb0b56aabbb9989f9d158b
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-02-23 14:33:41 +0100

    Implementation of the refactored methods, including using algorithms

--- game.cpp
+++ game.cpp
@@ -1,71 +1,52 @@
 #include "game.h"
+#include <algorithm>
 
 using namespace std;
 
-vector<string> platformNames{ "Nintendo", "Sony", "Microsoft", "PC" };
+const vector<string> platformNames{ "Nintendo", "Sony", "Microsoft", "PC" };
 
 Game::Game(const string& name, const int& releaseYear, const int& score, const vector<Platform>& platforms)
     : name{ name }, releaseYear{ releaseYear }, score{ score }, supportedPlatforms{ platforms }
 {
-    if (name.empty() || platforms.empty() || releaseYear < 1981 || releaseYear > 2019 || score < 0 || score > 10) {
-        throw runtime_error("Error!");
+    if (name.empty() || platforms.empty() || releaseYear < 1981 || releaseYear > 2026 || score < 0 || score > 10) {
+        throw std::runtime_error("Invalid game data");
     }
 }
 
-vector<Platform> Game::getPlatforms() const {
+const vector<Platform>& Game::getPlatforms() const noexcept {
     return supportedPlatforms;
 }
 
 bool Game::supportsPlatform(Platform p) const {
-    for (size_t i = 0; i < supportedPlatforms.size(); i++) {
-        if (p == supportedPlatforms.at(i)) return true;
-    }
-    return false;
+    return find(supportedPlatforms.begin(), supportedPlatforms.end(), p) != supportedPlatforms.end();
 }
 
-int Game::addPlatforms(const vector<Platform>& morePlatforms) {
+int Game::addPlatforms(const vector<Platform>& newPlatforms) {
 
-    int addedCount = 0;
-    
-    vector<Platform> toAdd;
-    
-    bool found = false;
+    int added = 0;
 
-    for (int i = 0; i < static_cast<int>(morePlatforms.size()); i++){
-
-        found = false;
-
-        for (int j = 0; j < static_cast<int>(supportedPlatforms.size()); j++) {
-            if (morePlatforms.at(i) == supportedPlatforms.at(j)) found = true;
-        }
-
-        if (!found) {
-            toAdd.push_back(morePlatforms.at(i));
-            addedCount++;
+    for(Platform p : newPlatforms){
+        if(!supportsPlatform(p)){
+            supportedPlatforms.push_back(p);
+            added++;
         }
     }
-
-    for (const auto& platform : toAdd){
-        supportedPlatforms.push_back(platform);
-    }
-
-    return addedCount;
+    return added;
 }
 
-bool operator>(const Game& left, const Game& right) {
-    return (left.score >= right.score && left.releaseYear > right.releaseYear);
+bool operator>(const Game& lhs, const Game& rhs) {
+    return (lhs.score >= rhs.score && lhs.releaseYear > rhs.releaseYear);
 }
 
 vector<Game> Game::filterByPlatform(const vector<Game>& games, Platform p) {
 
     vector<Game> result;
 
-    for (size_t i = 0; i < games.size(); i++) {
-        if (games.at(i).supportsPlatform(p)){
-            result.push_back(games.at(i));
+    for (const auto& g : games) {
+        if (g.supportsPlatform(p)){
+            result.push_back(g);
         }
     }
-
     return result;
 }
 
@@ -74,9 +55,9 @@ ostream& operator<<(ostream& os, const Game& game) {
     os << "[" << game.name << ", " << game.releaseYear << ", " << game.score << ", {";
 
     for (size_t i = 0; i < game.supportedPlatforms.size(); i++) {
-        os << platformNames.at(static_cast<int>(game.supportedPlatforms.at(i)));
+        os << platformNames.at(static_cast<int>(game.supportedPlatforms[i]));
 
-        if (i != game.supportedPlatforms.size() - 1){
+        if (i + 1 < game.supportedPlatforms.size()){
             os << ", ";
         }
     }
